c3cfeb7fb9c0ca95d3d732a6db396777a05fb468
yerzakovakmal <akmal63337@gmail.com>
2026-01-12 11:26:15 +0500

    added a player

--- python-projects/asteroidsgame/main.py
+++ python-projects/asteroidsgame/main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -9,15 +10,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 
 
 
